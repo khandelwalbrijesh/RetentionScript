42647ab9b87096b625e248219182c0e6b438e1c2
Brijesh Khandelwal <brkhande@microsoft.com>
2018-06-19 13:53:53 +0530

    Powershell module files added./

---
+++ RetentionScript.psm1
@@ -0,0 +1,126 @@
+Function Start-RetentionScript
+{
+[CmdletBinding(PositionalBinding = $false)]
+param (
+    [Parameter(Mandatory=$false)]
+    [String] $UserName,
+
+    [Parameter(Mandatory=$false)]
+    [String] $FileSharePath,
+
+    [Parameter(Mandatory=$true)]
+    [String] $StorageType,
+
+    [Parameter(Mandatory=$true)]
+    [String] $DateTimeBefore,
+    
+    [Parameter(Mandatory=$false)]
+    [String] $ConnectionString,
+
+    [Parameter(Mandatory=$false)]
+    [String] $ContainerName,
+
+    [Parameter(Mandatory=$false)]
+    [string] $StorageAccountName,
+
+    [Parameter(Mandatory=$false)]
+    [String] $StorageAccountKey,
+
+    [Parameter(Mandatory=$true)]
+    [String] $ClusterEndPoint,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $Force,
+
+    [Parameter(Mandatory=$false)]
+    [String] $PartitionId,
+
+    [Parameter(Mandatory=$false)]
+    [String] $ServiceId,
+ 
+    [Parameter(Mandatory=$false)]
+    [String] $ApplicationId,
+
+    [Parameter(Mandatory=$false)]
+    [String] $SSLCertificateThumbPrint
+)
+
+$command = ""
+if($StorageType -eq "FileShare")
+{
+  if(!$FileSharePath)
+  {
+    $FileSharePath = Read-Host -Prompt "Please enter the FileSharePath"
+  }
+
+  if($UserName)
+  {
+      $command = $command +  "Start-RetentionScriptFileShare -UserName `"$UserName`" -FileSharePath `"$FileSharePath`" -DateTimeBefore `"$DateTimeBefore`" -ClusterEndPoint `"$ClusterEndPoint`""
+  }
+  else {
+    $command = $command +  "Start-RetentionScriptFileShare -FileSharePath `"$FileSharePath`" -DateTimeBefore `"$DateTimeBefore`" -ClusterEndPoint `"$ClusterEndPoint`""
+  }
+}
+elseif($StorageType -eq "AzureBlob")
+{
+    if($ConnectionString)
+    {
+        if($ContainerName)
+        {
+            $command = $command + "Start-RetentionScriptAzureShare -ConnectionString `"$ConnectionString`" -DateTimeBefore `"$DateTimeBefore`" -ClusterEndPoint `"$ClusterEndPoint`""
+        }
+        else {
+            $command = $command + "Start-RetentionScriptAzureShare -ConnectionString `"$ConnectionString`" -DateTimeBefore `"$DateTimeBefore`" -ClusterEndPoint `"$ClusterEndPoint`""
+        }
+    }
+    else {
+        if(!$StorageAccountName)
+        {
+            $StorageAccountName = Read-Host -Prompt "Please enter the Storage account name"
+        }
+        if(!$StorageAccountKey)
+        {
+            $StorageAccountKey = Read-Host -Prompt "Please enter the Storage account key"
+        }
+        $command = $command + "Start-RetentionScriptAzureShare -StorageAccountName `"$StorageAccountName`" -StorageAccountKey `"$StorageAccountKey`" -DateTimeBefore `"$DateTimeBefore`" -ContainerName `"$ContainerName`" -ClusterEndPoint `"$ClusterEndPoint`""    
+    }
+
+    if($ContainerName)
+    {
+        $command = $command + " -ContainerName `"$ContainerName`""
+    }
+}
+else {
+    throw "The storage of type $StorageType not supported"
+}
+
+if($ApplicationId)
+{
+    Write-Host "ApplicationId is given."
+    $command = $command + " -ApplicationId `"$ApplicationId`""
+}
+if($ServiceId)
+{
+    $command = $command + " -ServiceId `"$ServiceId`""
+    Write-Host "Service is given"
+}
+
+if($SSLCertificateThumbPrint)
+{
+    $command = $command + " -SSLCertificateThumbPrint `"$SSLCertificateThumbPrint`""
+}
+
+if($PartitionId)
+{
+    $command = $command + " -PartitionId `"$PartitionId`""
+}
+
+if($Force)
+{
+    $command = $command + " -Force"    
+}
+
+Write-Host "Final Command to be executed : $command"
+$scriptBlock = [ScriptBlock]::Create($command)
+Invoke-Command $scriptBlock
+}
---
+++ UtilScript.psm1
@@ -0,0 +1,264 @@
+Function Get-PartitionDict 
+{    
+    [CmdletBinding(PositionalBinding = $false)]
+    param([Parameter(Mandatory=$true)][System.Collections.ArrayList]$pathsList
+    ) 
+
+    $partitionDict = New-Object 'system.collections.generic.dictionary[[string],[system.collections.generic.list[string]]]'
+    foreach($path in $pathsList)
+    {
+        $pathList = $path.Split("\",[StringSplitOptions]'RemoveEmptyEntries')
+        $length = $pathList.Count
+        $partitionID = $null
+        if($length -le 1)
+        {
+            $pathList = $path.Split("/",[StringSplitOptions]'RemoveEmptyEntries')
+            $length = $pathList.Count
+            if($length -le 1)
+            {
+                throw "$path is not in correct format."
+            }
+            Else
+            {
+                $partitionID = $pathList[$length - 2]
+            }
+        }
+        Else {
+            $partitionID = $pathList[$length - 2]            
+        }
+        
+    
+        if($partitionID -eq $null)
+        {
+            throw "Not able to extract partitionID"
+        }
+        
+        if(!$partitionDict.ContainsKey($partitionID))
+        {
+            Write-Host "Partition Id extracted is this $partitionID"
+            $partitionDict.Add($partitionID, $path)
+        }
+        else {
+            $partitionDict[$partitionID].add($path)
+        }
+    }
+
+    return $partitionDict
+}
+
+Function Get-FinalDateTimeBefore 
+{   
+    [CmdletBinding(PositionalBinding = $false)]    
+    param([Parameter(Mandatory=$true)][string]$DateTimeBefore, 
+    [Parameter(Mandatory=$true)][string]$Partitionid, 
+    [Parameter(Mandatory=$true)][string]$ClusterEndpoint,
+    [Parameter(Mandatory=$false)][bool]$Force,
+    [Parameter(Mandatory=$false)][string]$SSLCertificateThumbPrint
+    )  
+
+    # DateTime Improvement to be done here.
+    $dateTimeBeforeObject = [DateTime]::ParseExact($DateTimeBefore,"yyyy-MM-dd HH.mm.ssZ",[System.Globalization.DateTimeFormatInfo]::InvariantInfo,[System.Globalization.DateTimeStyles]::None)
+    $finalDateTimeObject = $dateTimeBeforeObject
+    $dateTimeBeforeString = $dateTimeBeforeObject.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ") 
+    $url = "http://$ClusterEndpoint/Partitions/$Partitionid/$/GetBackups?api-version=6.2-preview&EndDateTimeFilter=$dateTimeBeforeString"
+    $backupEnumerations = $null
+    try {
+        Write-Host "Querying the URL: $url"
+        if($SSLCertificateThumbPrint)
+        {
+            $url = "https://$ClusterEndpoint/Partitions/$Partitionid/$/GetBackups?api-version=6.2-preview&EndDateTimeFilter=$dateTimeBeforeString"
+            Write-Host "Querying the URL: $url"    
+            $pagedBackupEnumeration = Invoke-RestMethod -Uri $url  -CertificateThumbprint $SSLCertificateThumbPrint
+        }
+        else {  
+            Write-Host "Trying to query without cert thumbprint"
+            Write-Host "Querying the URL: $url"
+            $pagedBackupEnumeration = Invoke-RestMethod  -Uri $url       
+        }
+        Write-Host "Sorting the list of backupEnumerations with respect to creationTimeUtc."
+        $backupEnumerations = $pagedBackupEnumeration.Items | Sort-Object -Property @{Expression = {[DateTime]::ParseExact($_.CreationTimeUtc,"yyyy-MM-ddTHH:mm:ssZ",[System.Globalization.DateTimeFormatInfo]::InvariantInfo,[System.Globalization.DateTimeStyles]::None)}; Ascending = $false}
+    }
+    catch  {
+        $err = $_.ToString() | ConvertFrom-Json
+        if($err.Error.Code -eq "FABRIC_E_PARTITION_NOT_FOUND")
+        {
+            Write-Host "$Partitionid is not found." 
+            if($Force -eq $true)
+            {
+                Write-Host "Force flag is enabled so, deleting data all in this partition"
+                return [DateTime]::MaxValue
+            }
+            else {
+                Write-Host "If you want to delete the data in this partition."
+                Write-Host "If you want to remove this partition as well, please run the script by enabling force flag."
+                return [DateTime]::MinValue
+            }
+        }
+        else {
+            throw $_.Exception.Message
+        }
+    }
+
+    Write-Host "Finding the finalDateTime in backupEnumerations."
+    Write-Host "Iterating over backupEnumerations till we find the last full backup"
+    $fullBackupFound = $false
+    foreach($backupEnumeration in $backupEnumerations)
+    {
+        Write-Host $backupEnumeration.BackupType
+        if($backupEnumeration.BackupType -eq "Full")
+        {
+            Write-Host "Full backup is found."
+            $finalDateTimeObject = [DateTime]::Parse($backupEnumeration.CreationTimeUtc)
+            $fullBackupFound = $true
+            break
+        }
+    }
+    if($backupEnumerations.Count -eq 0)
+    {
+        Write-Host "The BackupEnumerations had length equal to 0. So, could not go through with the cleanup for this partition: $Partitionid"
+        return [DateTime]::MinValue
+    }
+
+    if(!$fullBackupFound)
+    {
+        Write-Host "The Backups Before this $dateTimeBeforeString date are corrupt as no full backup is found, So, deleting them."
+    }
+    return $finalDateTimeObject
+}
+
+
+Function Get-PartitionIdList 
+{   
+    [CmdletBinding(PositionalBinding = $false)]
+    param([Parameter(Mandatory=$false)][string]$ApplicationId, 
+    [Parameter(Mandatory=$false)][string]$ServiceId,
+    [Parameter(Mandatory=$true)][string]$ClusterEndpoint,    
+    [Parameter(Mandatory=$false)][string]$SSLCertificateThumbPrint
+    ) 
+    # need to add continuationToken Logic here.
+    $serviceIdList = New-Object System.Collections.ArrayList
+    if($ApplicationId)
+    {
+        if($SSLCertificateThumbPrint)
+        {
+            $serviceIdList = Get-ServiceIdList -ApplicationId $ApplicationId -ClusterEndpoint $ClusterEndpoint -SSLCertificateThumbPrint $SSLCertificateThumbPrint
+        }
+        else {
+            $serviceIdList = Get-ServiceIdList -ApplicationId $ApplicationId -ClusterEndpoint $ClusterEndpoint
+        }
+    }
+    else {
+        $serviceIdList.Add($ServiceId) | Out-Null
+    }
+
+    $partitionIdList = New-Object System.Collections.ArrayList
+
+    foreach($serviceId in $serviceIdList)
+    {
+        Write-Host " Service Id found: $serviceId"
+        $continuationToken = $null
+        do
+        {
+            if($SSLCertificateThumbPrint)
+            {
+                $partitionInfoList = Invoke-RestMethod -Uri "https://$ClusterEndpoint/Services/$serviceId/$/GetPartitions?api-version=6.2&ContinuationToken=$continuationToken"  -CertificateThumbprint $SSLCertificateThumbPrint
+            }
+            else {  
+                Write-Host "Trying to query without cert thumbprint"
+                $partitionInfoList = Invoke-RestMethod -Uri "http://$ClusterEndpoint/Services/$serviceId/$/GetPartitions?api-version=6.2&ContinuationToken=$continuationToken" 
+            }
+            foreach($partitionInfo in $partitionInfoList.Items)
+            {
+                $partitionid = $partitionInfo.PartitionInformation.Id
+                Write-Host "$partitionid"    
+                $partitionIdList.Add($partitionInfo.PartitionInformation.Id)
+            }
+            $continuationToken = $partitionInfoList.ContinuationToken
+        }while($continuationToken -ne "")
+    }
+    $length = $partitionIdList.Count
+    Write-Host "The total number of partitions found are $length"
+    return $partitionIdList
+}
+
+
+Function Get-ServiceIdList 
+{   
+    [CmdletBinding(PositionalBinding = $false)]
+    param([Parameter(Mandatory=$true)][string]$ApplicationId,
+    [Parameter(Mandatory=$true)][string]$ClusterEndpoint,    
+    [Parameter(Mandatory=$false)][string]$SSLCertificateThumbPrint
+        )
+
+    $continuationToken = $null
+    $serviceIdList = New-Object System.Collections.ArrayList
+    do
+    {
+        if($SSLCertificateThumbPrint)
+        {
+            $serviceInfoList = Invoke-RestMethod -Uri "https://$ClusterEndpoint/Applications/$ApplicationId/$/GetServices?api-version=6.2&ContinuationToken=$continuationToken" -CertificateThumbprint $SSLCertificateThumbPrint
+        }
+        else {  
+            Write-Host "Trying to query without cert thumbprint"
+            $serviceInfoList = Invoke-RestMethod -Uri "http://$ClusterEndpoint/Applications/$ApplicationId/$/GetServices?api-version=6.2&ContinuationToken=$continuationToken"
+        }
+        foreach($serviceInfo in $serviceInfoList.Items)
+        {
+            $serviceIdList.Add($serviceInfo.Id) | Out-Null
+            $serviceId = $serviceInfo.Id
+            Write-Host "$serviceId"
+        }
+        $continuationToken = $serviceInfoList.ContinuationToken
+    }while($continuationToken -ne "")
+
+    $length = $serviceIdList.Count
+    Write-Host "$ApplicationId has $length number of services"
+    return $serviceIdList
+}
+
+
+Function Start-BackupDataCorruptionTest 
+{  
+    [CmdletBinding(PositionalBinding = $false)]
+    param([Parameter(Mandatory=$true)][string]$DateTimeBefore,
+    [Parameter(Mandatory=$true)][string]$Partitionid, 
+    [Parameter(Mandatory=$true)][string]$ClusterEndpoint,
+    [Parameter(Mandatory=$false)][string]$SSLCertificateThumbPrint
+    )
+    $dateTimeBeforeObject = [DateTime]::ParseExact($DateTimeBefore,"yyyy-MM-dd HH.mm.ssZ",[System.Globalization.DateTimeFormatInfo]::InvariantInfo,[System.Globalization.DateTimeStyles]::None)
+    $finalDateTimeObject = $dateTimeBeforeObject
+    $dateTimeBeforeString = $dateTimeBeforeObject.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ") 
+    # DateTime Improvement to be done here.
+    $url = "http://$ClusterEndpoint/Partitions/$Partitionid/$/GetBackups?api-version=6.2-preview&EndDateTimeFilter=$dateTimeBeforeString"
+    Write-Host "$url"
+    
+    $backupEnumerations = $null
+    try {
+        if($SSLCertificateThumbPrint)
+        {
+            $url = "https://$ClusterEndpoint/Partitions/$Partitionid/$/GetBackups?api-version=6.2-preview&EndDateTimeFilter=$dateTimeBeforeString"
+            Write-Host "Querying the URL: $url"
+            $pagedBackupEnumeration = Invoke-RestMethod -Uri $url -CertificateThumbprint  $SSLCertificateThumbPrint
+        }
+        else {
+            Write-Host "Querying the URL: $url"
+            $pagedBackupEnumeration = Invoke-RestMethod -Uri $url 
+        }
+        $backupEnumerations = $pagedBackupEnumeration.Items | Sort-Object -Property @{Expression = {[DateTime]::ParseExact($_.CreationTimeUtc,"yyyy-MM-ddTHH:mm:ssZ",[System.Globalization.DateTimeFormatInfo]::InvariantInfo,[System.Globalization.DateTimeStyles]::None)}; Ascending = $true}
+        
+        if($backupEnumerations -ne $null -and $backupEnumerations[0].BackupType -ne "Full")
+        {
+            throw "Data is corrupted for this partition : $Partitionid"
+        }
+    }
+    catch  {
+        $err = $_.ToString() | ConvertFrom-Json
+        if($err.Error.Code -eq "FABRIC_E_PARTITION_NOT_FOUND")
+        {
+            Write-Host "Partition not found, so, could not go through with testing the integrity of data of this partition."
+        }
+        else {
+            throw $_.Exception.Message
+        }
+    }
+}
